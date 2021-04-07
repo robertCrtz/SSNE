82efc828bf2e0d7333f566fe8f6f5e53b6053506
robertCrtz <robertoenrique98@hotmail.com>
2021-04-07 15:28:20 -0600

    modifying the component form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
+import { FormComponent } from './components/form/form.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 
@@ -8,7 +9,8 @@ const routes: Routes = [
   { path: ''     , redirectTo: 'home', pathMatch: 'full' },
   { path: 'home' , component: HomeComponent },
   { path: 'projects' , component: ProjectsComponent },
-  { path: 'contact' , component: ContactComponent }
+  { path: 'contact' , component: ContactComponent },
+  { path: 'request' , component: FormComponent }
 ];
 
 @NgModule({
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  submitted = false;
 
+ constructor(private formBuilder: FormBuilder) { 
+   this.buildForm();
+  }
+
+  
   ngOnInit(): void {
+  }
+
+  private buildForm(){
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
+      text: ['', [Validators.maxLength(100), Validators.required]],
+      category: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(){
+    this.submitted = true;
+
+    if(this.form.invalid) {
+      return;
+    }
+  }
+
+  get f(){
+    return this.form.controls;
   }
 
 }
