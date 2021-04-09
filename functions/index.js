95d9d9ca11a115c3ca98c2dcde7771892a5a256d
robertCrtz <robertoenrique98@hotmail.com>
2021-04-09 11:49:36 -0600

    Editing template from home and contact

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,50 @@
 const functions = require("firebase-functions");
+const nodemailer = require ("nodemailer");
+const express = require('express');
+const cors = require('cors');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+if(process.env.NODE_ENV !="production" ){
+    require('dotenv').config()
+}
+
+const app = express();
+app.use(cors({ origin:true }));
+
+
+app.post('/', (req, res) =>{
+    const {body} = req;
+    const isValidMessage = body.message && body.to && body.subject;
+
+    if(!isValidMessage){
+        return res.status(400).send ({message: 'Invalid request'});
+    }
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "robertelias3306@gmail.com",
+            pass: "uzlnjbsqogngqcht"
+        }
+    });
+
+    //email to send
+    const mailOptions = {
+        from: "robertelias3306@gmail.com",
+        to: "robertelias3306@gmail.com",
+        subject: "Interested customer!",
+        html: `
+            <b>Hello world</b>
+            ${this.name}
+        `
+    };
+
+    transporter.sendMail(mailOptions, (err, data) =>{
+        if(err){
+            return res.status(500).send({ message: "error "+ err.message });
+        }
+
+        return res.send ({ message: "email sent" });
+    });
 });
+
+module.exports.mailer = functions.https.onRequest(app);
